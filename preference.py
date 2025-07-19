cbf1ba9059b23f0f16ebb5f2303412f2d29e3bb9
CantaTam <cantande@foxmail.com>
2025-07-20 05:03:55 +0800

    睡前备份

--- preference.py
+++ preference.py
@@ -1,37 +1,39 @@
 import bpy
 from . import ADDON_NAME
 
+def not_empty_suffix(self, context):
+    if self.custom_suffix.strip() == "":
+        self.custom_suffix = "BAK"
+
 class BA_OT_preference(bpy.types.AddonPreferences):
     bl_idname = ADDON_NAME
 
     custom_suffix:bpy.props.StringProperty(
         name="",
-        description="自定义备份对象的名称的后缀名",
-        default="BAK"
+        description="备份对象的名称的后缀名，留空则使用默认值",
+        default="BAK",
+        update=not_empty_suffix,
     )
 
     backup_mode: bpy.props.EnumProperty(
         name="",
-        description="选择默认的备份模式",
         items=[
-            ('OVERWRITE',   "覆盖备份", "用当前状态覆盖原备份", 'MESH_PLANE', 0),
-            ('INCREMENTAL', "增量备份", "只保存更改的部分", 'CUBE', 1),
+            ('OVERWRITE',   "覆盖备份", "用当前状态覆盖原备份"),
+            ('INCREASE', "增量备份", "只保存更改的部分"),
         ],
         default='OVERWRITE',
 )
     
     def draw(self, context):
         layout = self.layout
-        split = layout.row().split(factor=0.2)
+        split = layout.split(factor=0.2)
 
         col_left = split.column()
         col_left.alignment = 'LEFT'
         col_right = split.column()
 
         col_left.label(text="备份后缀")
-        row = col_right.row()
-        row.prop(self, "custom_suffix")
+        col_right.prop(self, "custom_suffix")
 
         col_left.label(text="备份模式")
-        row = col_right.row()
-        row.prop(self, "backup_mode")
+        col_right.prop(self, "backup_mode")
--- show_backup_button.py
+++ show_backup_button.py
@@ -1,9 +1,13 @@
 from . import load_custom_icons
+from . import ADDON_NAME
 
 def draw_backup_assistant_button(self, context):
     layout = self.layout
 
+    prefs = context.preferences.addons[ADDON_NAME].preferences
+
     if context.area.type == 'OUTLINER':
-        row = layout.row(align=True) 
-        row.operator("bak.increase_backup", text="", icon='CUBE')
-        row.operator("bak.increase_backup", text="自定图标", icon_value=load_custom_icons.custom_icons["INCREASE_BACKUP"].icon_id)
+        if prefs.backup_mode == "OVERWRITE":
+            layout.operator("bak.overwrite_backup", text="", icon_value=load_custom_icons.custom_icons["OVERWRITE_BACKUP"].icon_id)
+        elif prefs.backup_mode == "INCREASE":
+            layout.operator("bak.increase_backup", text="", icon_value=load_custom_icons.custom_icons["INCREASE_BACKUP"].icon_id)
