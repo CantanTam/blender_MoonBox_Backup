78be1e8063cfd25f17d2149d9e4d58f419da5cc2
CantaTam <cantande@foxmail.com>
2025-07-25 00:05:55 +0800

    添加vertex edge polygon的hash判定条件

--- addon_property.py
+++ addon_property.py
@@ -5,6 +5,9 @@ class BA_PG_backup_object(bpy.types.PropertyGroup):
     backup_object_name: bpy.props.StringProperty(default="")
     # 用于存储编辑变化状态
     backup_transform_hash: bpy.props.StringProperty(default="")
+    backup_modifier_count: bpy.props.IntProperty(default=0)
+    backp_vef_hash:bpy.props.StringProperty(default="")
+
     backup_modifier_hash: bpy.props.StringProperty(default="")
 
 class BA_PG_backup_object_list(bpy.types.PropertyGroup):
--- func_detect_change.py
+++ func_detect_change.py
@@ -7,11 +7,34 @@ def is_edit_change():
 
     for item in edit_change_object_list:
         if item.backup_object_name == edit_object.name:
-            object_matrix = edit_object.matrix_world
-            object_matrix_tuple = tuple(round(v,6) for row in object_matrix for v in row)
-            edit_transform_hash = str(hash(object_matrix_tuple))
 
-            if item.backup_transform_hash != edit_transform_hash:
-                item.backup_transform_hash = edit_transform_hash
+            # 通过物体形变的与否来判定编辑状态
+            if item.backup_transform_hash != str(hash(tuple(round(v,6) for row in edit_object.matrix_world for v in row))):
+                item.backup_transform_hash = str(hash(tuple(round(v,6) for row in edit_object.matrix_world for v in row)))
                 return True
+            
+            # 通过添加的修改器数量差别来判定编辑状态
+            elif item.backup_modifier_count != len(bpy.context.object.modifiers):
+                item.backup_modifier_count = len(bpy.context.object.modifiers)
+                return True
+            
+            # 通过点/线/面各自的值组
+            elif item.backp_vef_hash != str(hash((
+                    len(bpy.context.active_object.data.vertices),
+                    len(bpy.context.active_object.data.edges),
+                    len(bpy.context.active_object.data.polygons),
+                    ))):
+                item.backp_vef_hash = str(hash((
+                    len(bpy.context.active_object.data.vertices),
+                    len(bpy.context.active_object.data.edges),
+                    len(bpy.context.active_object.data.polygons),
+                    )))
+                return True
+
+            else:
+                return False
+            
+
+
+
             
