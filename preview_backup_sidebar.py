31edba6900282bd0036bfc5ff427cca3ca67f73f
CantaTam <cantande@foxmail.com>
2025-07-29 12:33:21 +0800

    snapshot预览功能暂时可用

--- preview_backup_sidebar.py
+++ preview_backup_sidebar.py
@@ -12,16 +12,25 @@ def load_backup_snapshots():
         backup_snapshots = bpy.utils.previews.new()
 
     snapshot_dir = os.path.join(os.path.dirname(__file__), "backup_snapshots")
-    snapshot_path = os.path.join(snapshot_dir, "test.jpg")
 
-    if os.path.exists(snapshot_path):
-        backup_snapshots.load("my_icon", snapshot_path, 'IMAGE')
+    for filename in os.listdir(snapshot_dir):
+        if filename.lower().endswith(".jpg"):
+            snapshot_name = os.path.splitext(filename)[0]
+            snapshot_path = os.path.join(snapshot_dir, filename)
+            backup_snapshots.load(snapshot_name, snapshot_path, 'IMAGE')
+
+    #snapshot_path = os.path.join(snapshot_dir, "test.jpg")
+
+    #if os.path.exists(snapshot_path):
+        #backup_snapshots.load("my_icon", snapshot_path, 'IMAGE')
 
 def clear_backup_snapshots():
     global backup_snapshots
+    global is_snapshot_loaded
     if backup_snapshots is not None:
         bpy.utils.previews.remove(backup_snapshots)
         backup_snapshots = None
+        is_snapshot_loaded = False
 
 class VIEW3D_PT_icon_image_panel(bpy.types.Panel):
     bl_label = "Icon Image Example"
@@ -36,10 +45,13 @@ class VIEW3D_PT_icon_image_panel(bpy.types.Panel):
             load_backup_snapshots()
             is_snapshot_loaded = True
 
+        current_backup = bpy.context.active_object
+        current_snapshot = current_backup.ba_data.object_uuid + "_" + current_backup.ba_data.backup_uuid
+
         layout = self.layout
-        if backup_snapshots and "my_icon" in backup_snapshots:
+        if backup_snapshots and "20250729120246_PLdZgw" in backup_snapshots:
             box = layout.box()  # 创建一个带边框的区域
-            box.template_icon(icon_value=backup_snapshots["my_icon"].icon_id, scale=10)
+            box.template_icon(icon_value=backup_snapshots["20250729120246_PLdZgw"].icon_id, scale=10)
         else:
             layout.label(text="未加载图标")
         layout.operator("transform.translate", icon="CUBE", text="")
